fc8b2bb67db8a6bb75976b7d8af4853374992219
Swaroop Sridhar <sridhs2@rpi.edu>
2025-06-27 14:09:47 -0400

    removed commented lines of code in rotate and reflect function

--- XPointMLTest.py
+++ XPointMLTest.py
@@ -77,7 +77,6 @@ def rotate(frameData,deg):
     plotSimple(all[2], f"{frameData['fnum']}_rotation{deg}_all2.png")
     plotSimple(all[3], f"{frameData['fnum']}_rotation{deg}_all3.png")
     
-    # mask = v2.functional.rotate(frameData["mask"], deg, v2.InterpolationMode.BILINEAR)
     mask = v2.functional.rotate(frameData["mask"], deg, v2.InterpolationMode.NEAREST)
     return {
         "fnum": frameData["fnum"],
@@ -96,11 +95,7 @@ def reflect(frameData,axis):
     if axis not in [0,1]:
         print(f"invalid reflection axis specified... exiting")
         sys.exit()
-        
-    # psi = torch.flip(frameData["psi"][0], dims=(axis,)).unsqueeze(0)
-    # all = torch.flip(frameData["all"], dims=(axis,))
-    # mask = torch.flip(frameData["mask"][0], dims=(axis,)).unsqueeze(0)
-    
+            
     psi = torch.flip(frameData["psi"], dims=(axis+1,))
     all = torch.flip(frameData["all"], dims=(axis+1,))
     mask = torch.flip(frameData["mask"], dims=(axis+1,))
